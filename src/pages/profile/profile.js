5f88ec52afe332c897dc56417d0ffb9f6b8cadd0
ViniciusOkaeda <viniciusokaeda@gmail.com>
2024-11-04 12:30:37 -0300

    ajuste de navegacao

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -7,15 +7,16 @@ import { Logout } from "../../utils/constants";
 import { useNavigate } from "react-router-dom";
 
 function Profile() {
-    const [containerCount, setContainerCount] = useState(-1);
-    const [cardCount, setCardCount] = useState(0);
+    const [containerCount, setContainerCount] = useState(-1); // Começa em 0 para o primeiro elemento
+    const [cardCount, setCardCount] = useState(0); // Começa em 0 para o primeiro elemento
     const [selectableContainers, setSelectableContainers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true); // Inicialmente, estamos carregando
     const [error, setError] = useState('');
     const [profiles, setProfiles] = useState([]);
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const SaveProfileData = (profiles_name, profiles_id, profile_image) => {
+
+    const SaveProfileData = (profiles_name, token, profiles_id, profile_image) => {
         sessionStorage.setItem("profileid", btoa(profiles_id));
         localStorage.setItem("profileimage", profile_image);
         localStorage.setItem("profilename", profiles_name);
@@ -26,27 +27,28 @@ function Profile() {
         const loadData = async () => {
             try {
                 const result = await LoginMotvWithToken();
-                if (result && result.status === 1) {
-                    setProfiles(result.response);
+                if (result) {
+                    if(result.status === 1) {
+                        setProfiles(result.response)
+                    }
                 }
             } catch (err) {
                 setError(err.message || 'An error occurred');
             } finally {
-                setLoading(false);
+                setLoading(false); // Dados carregados, então setar como false
             }
         };
 
         loadData(); // Chama a função de carregamento ao montar o componente
-    }, []); // Este useEffect será executado apenas uma vez
+
+    }, []); // Dependência vazia para garantir que loadData só seja chamado uma vez
 
     useEffect(() => {
         const selectableContainer = document.querySelectorAll('.selectedContainer');
         setSelectableContainers(selectableContainer);
-    }, []); // Executa apenas uma vez após a montagem
-
-    useEffect(() => {
+        // Configuração do keyDownHandler
         const keyDownHandler = (event) => {
-            if (!loading) {
+            if (!loading) { // Apenas permite navegação se não estiver carregando
                 handleKeyDown(event, {
                     enter: () => {
                         const focusedElement = document.activeElement;
@@ -54,12 +56,13 @@ function Profile() {
                             const index = Array.from(document.querySelectorAll('.profileButton')).indexOf(focusedElement);
                             SaveProfileData(
                                 profiles.profiles[index]?.profiles_name,
+                                profiles.profiles[index]?.token,
                                 profiles.profiles[index]?.profiles_id,
                                 profiles.profiles[index]?.image
-                            );
+                            )
                             console.log("Dados do perfil:", profiles.profiles[index]);
                         } else if (focusedElement.id === "logoutButton") {
-                            Logout(navigate);
+                            Logout(navigate)
                         }
                     },
                     escape: () => {
@@ -69,7 +72,8 @@ function Profile() {
                         if (containerCount > 0) {
                             setContainerCount(prev => {
                                 const newCount = prev - 1;
-                                setCardCount(0);
+                                setCardCount(0)
+                                // Focar no card anterior
                                 selectableContainers[newCount]?.getElementsByClassName('selectedCard')[0]?.focus();
                                 return newCount;
                             });
@@ -79,28 +83,36 @@ function Profile() {
                         if (containerCount < selectableContainers.length - 1) {
                             setContainerCount(prev => {
                                 const newCount = prev + 1;
+                                // Focar no próximo card (resetando cardCount se necessário)
                                 selectableContainers[newCount]?.getElementsByClassName('selectedCard')[0]?.focus();
                                 return newCount;
                             });
                         }
                     },
                     left: () => {
-                        if (containerCount === 0 && cardCount > 0) {
-                            setCardCount(prev => {
-                                const newCardCount = prev - 1;
-                                selectableContainers[0]?.getElementsByClassName('selectedCard')[newCardCount]?.focus();
-                                return newCardCount;
-                            });
+                        if(containerCount === 0 ) {
+                            if(cardCount > 0) {
+                                setCardCount(prev => {
+                                    const newCardCount = prev - 1;
+                                    selectableContainer[0]?.getElementsByClassName('selectedCard')[newCardCount]?.focus()
+                                    return newCardCount;
+                                })
+                            }
                         }
                     },
                     right: () => {
-                        if (containerCount === 0 && cardCount < selectableContainers[0]?.getElementsByClassName('selectedCard').length - 1) {
-                            setCardCount(prev => {
-                                const newCardCount = prev + 1;
-                                selectableContainers[0]?.getElementsByClassName('selectedCard')[newCardCount]?.focus();
-                                return newCardCount;
-                            });
+                        if(containerCount === 0 ) {
+                            if(cardCount < selectableContainers[0]?.getElementsByClassName('selectedCard').length -1) {
+                                setCardCount(prev => {
+                                    const newCardCount = prev + 1;
+                                    selectableContainer[0]?.getElementsByClassName('selectedCard')[newCardCount]?.focus()
+                                    return newCardCount;
+                                })
+                                //terminar aqui
+                            }
+                            //console.log("VEJAMOS", )
                         }
+                        //console.log("Right key pressed on Page 1", selectableContainers[0]?.getElementsByClassName('selectedCard'));
                     },
                     home: () => {
                         console.log("Home key pressed on Page 1");
@@ -108,6 +120,7 @@ function Profile() {
                     squares: () => {
                         console.log("Squares key pressed on Page 1");
                     },
+                    // Outras teclas...
                 });
             }
         };
@@ -116,37 +129,54 @@ function Profile() {
         return () => {
             document.removeEventListener("keydown", keyDownHandler);
         };
-    }, [loading, containerCount, cardCount, profiles, selectableContainers]); // Dependências mantidas
+
+    }, [containerCount, cardCount, profiles]); // Dependência vazia para garantir que loadData só seja chamado uma vez
 
     return (
         <>
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <div className="initialContainer"><h3>Erro: {error}</h3></div>
-            ) : (
-                <div className="flex container declaredHeight flexColumn">
-                    <div className="headerContainer">
-                        <h2>Quem está assistindo?</h2>
-                    </div>
+        {loading ? (
+            <Loader /> // Exibe o Loader enquanto carrega
+        ) : error ? (
+            <div className="initialContainer"><h3>Erro: {error}</h3></div> // Exibindo mensagem de erro, se houver
 
-                    <div className="profileContainer selectedContainer">
-                        {profiles.profiles.map((item, idx) => (
-                            <div key={idx} className="profileContent">
-                                <button className="profileButton selectedCard">
-                                    <img src={item.image} alt={item.profiles_name} />
-                                </button>
-                                <h4>{item.profiles_name}</h4>
-                            </div>
-                        ))}
-                    </div>
 
-                    <div className="footerContainer selectedContainer">
-                        <button id="logoutButton" className="footerButton selectedCard">Sair</button>
-                    </div>
+        ) : (
+            <div className="flex container declaredHeight flexColumn">
+                <div className="headerContainer">
+                    <h2>Quem está assistindo?</h2>
                 </div>
-            )}
+
+                <div className="profileContainer selectedContainer">
+
+                    {profiles.profiles.map((item, idx) => {
+
+                        return(
+
+                    <div key={idx} className="profileContent">
+                        <button className="profileButton selectedCard">
+                            <img src={item.image}></img>
+                        </button>
+
+                        <h4>{item.profiles_name}</h4>
+                    </div>
+
+                        )
+                    })}
+
+
+
+                </div>
+
+                <div className="footerContainer selectedContainer">
+                    <button id="logoutButton" className="footerButton selectedCard">Sair</button>
+                </div>
+            </div>
+        )
+    
+    }
+        
         </>
+
     );
 }
 
