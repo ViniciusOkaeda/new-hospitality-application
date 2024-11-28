87b0b8f3716301759b89a85ebae4971f4d572454
ViniciusOkaeda <viniciusokaeda@gmail.com>
2024-11-28 17:42:18 -0300

    updates detailcatchup

--- src/pages/catchup/detailCatchup.js
+++ src/pages/catchup/detailCatchup.js
@@ -27,18 +27,37 @@ function DetailCatchup() {
     const [channelsName, setChannelsName] = useState('')
     const [channelsImage, setChannelsImage] = useState('')
     const [focusedContent, setFocusedContent] = useState([])
-    const [channelFocused, setChannelFocused] = useState([])
+
+    const [dateIndex, setDateIndex] = useState(0)
+    const [dateFocused, setDateFocused] = useState(0)
+    const [individualDataLength, setIndividualDataLength] = useState(0)
+    console.log("vejamoso valor", dateFocused)
+
+
+    const handleFocus = (idx) => {
+        // Calcular o totalPreviousDataLength com base no índice atual
+        const calculatedLength = eventChannels
+          .slice(0, idx)  // Pega todos os itens antes do índice atual
+          .reduce((acc, currentItem) => acc + currentItem.data.length, 0);
+    
+        // Atualiza o estado com o comprimento calculado
+        setDateFocused(calculatedLength);
+    
+        // Define o índice do botão que recebeu o foco
+        setDateIndex(idx);
+      };
 
     const [haveFocusedEvent, setHaveFocusedEvent] = useState(false);
     const [eventChannels, setEventChannels] = useState([])
+    const [hasFocused, setHasFocused] = useState(false);
 
-    const divRef = useRef([[], []]);    const handleButtonRef = (index, index2, el) => {
-        // Garantir que o array esteja inicializado
-        if (!divRef.current[index]) {
-            divRef.current[index] = [];
-        }
-        divRef.current[index][index2] = el;
-    };
+    const divRef = useRef([[], []]);    
+    const scrollRef = useRef([[], []])
+    const dateFocusedRef = useRef(dateFocused);
+
+    React.useEffect(() => {
+        dateFocusedRef.current = dateFocused;
+    }, [dateFocused]);
 
     const handleEventButtonRef = (el) => {
         if (el) {
@@ -48,6 +67,16 @@ function DetailCatchup() {
           }
           if (!divRef.current[1].includes(el)) { // Adiciona a referência se não estiver presente
             divRef.current[1].push(el);
+            //console.log('Referência adicionada ao índice 1:', el);
+
+          }
+          if (!scrollRef.current[0]) {
+            scrollRef.current[0] = []; // Inicializa o índice 1 se estiver undefined
+          }
+          if (!scrollRef.current[0].includes(el)) { // Adiciona a referência se não estiver presente
+            scrollRef.current[0].push(el);
+            //console.log('Referência adicionada ao índice 1:', el);
+
           }
         }
       };
@@ -60,7 +89,7 @@ function DetailCatchup() {
           }
           if (!divRef.current[0].includes(el)) { // Adiciona a referência se não estiver presente
             divRef.current[0].push(el);
-            console.log('Referência adicionada ao índice 0:', el);
+            //console.log('Referência adicionada ao índice 0:', el);
           }
         }
       };
@@ -72,11 +101,25 @@ function DetailCatchup() {
         if (!divRef.current[1]) {
             divRef.current[1] = [];
         }
+        if (!scrollRef.current[0]) {
+            scrollRef.current[0] = [];
+        }
+
     }, []);
 
-
-    // Função para atualizar as dimensões da div
-
+    useEffect(() => {
+        // Só foca no primeiro elemento do índice 1 se ainda não tiver sido feito
+        if (divRef.current[1].length > 0 && !hasFocused) {
+            setContainerCount(1)
+            setCardCount(0)
+          const firstElement = divRef.current[1][0];
+          firstElement.focus();
+          console.log("Focado no primeiro elemento do índice 1:", firstElement);
+          
+          // Define a flag para garantir que o foco só aconteça uma vez
+          setHasFocused(true);
+        }
+      }, [divRef.current[1], hasFocused]);
 
     useEffect(() => {
         const loadData = async () => {
@@ -89,21 +132,16 @@ function DetailCatchup() {
                         const transformedData = transformData(result.response);
                         console.log("o transforfm", transformedData)
                         setEventChannels(transformedData.reverse())
-                        setContainerCount(1)
-                        setCardCount(-1)
+
 
                     }
                 }
             } catch (err) {
                 setError(err.message || 'An error occurred');
             } finally {
-                setTimeout(() => {
                     setLoading(false); // Dados carregados, então setar como false
                     setEnableArrows(true);
                     setContainerCount(1)
-
-                }, 1000)
-
 
             }
         };
@@ -118,15 +156,25 @@ function DetailCatchup() {
 
     const transformData = (events) => {
         const groupedByDay = {};
-
+    
+        // Obter a data e hora atuais para comparação
+        const now = new Date();
+    
         events.forEach((event) => {
-            const eventDate = new Date(event.start);  // Converte a string "start" para um objeto Date
-            const day = eventDate.toISOString().split('T')[0];  // Extrai a data sem o horário (ex: '2024-11-20')
-
+            const eventDate = new Date(event.start); // Converte a string "start" para um objeto Date
+            const endDate = new Date(event.end); // Converte a string "end" para um objeto Date
+    
+            // Exclui eventos que terminam no futuro
+            if (endDate > now) {
+                return; // Não adiciona eventos que terminam no futuro
+            }
+    
+            const day = eventDate.toLocaleDateString('en-GB'); // Exemplo: 'DD/MM/YYYY'
+    
             if (!groupedByDay[day]) {
                 groupedByDay[day] = [];  // Inicializa um array para o dia, se não existir
             }
-
+    
             // Adiciona o evento ao dia correspondente
             groupedByDay[day].push({
                 channels_id: event.channels_id,
@@ -139,74 +187,110 @@ function DetailCatchup() {
                 image_widescreen: event.image_widescreen,
                 rating: event.rating,
                 start: event.start,
+                end: event.end, // Inclui o "end" para poder compará-lo
                 title: event.title
-
             });
         });
-
+    
         // Formata o objeto final
-        const transformed = Object.keys(groupedByDay).map(day => ({
+        let transformed = Object.keys(groupedByDay).map(day => ({
             day: day,
             data: groupedByDay[day]
         }));
-
+    
+        // Inverte a ordem de todos os grupos
+        transformed.forEach(group => {
+            group.data.reverse();
+        });
+    
         return transformed;
     };
 
     const getDayLabel = (dateStr, completeDay) => {
         const today = new Date(); // Data atual
-        const dayDate = new Date(dateStr); // Data recebida
-
-        // Zera as horas, minutos, segundos e milissegundos para comparar as datas sem o horário
+        const [day, month, year] = dateStr.split('/'); // Divide a data "dd/mm/yyyy"
+        const dayDate = new Date(`${year}-${month}-${day}`); // Cria a data no formato yyyy-mm-dd para comparação
+    
+        // Zera as horas, minutos, segundos e milissegundos para comparar apenas a data
         today.setHours(0, 0, 0, 0);
         dayDate.setHours(0, 0, 0, 0);
-
+    
         const diffTime = today - dayDate; // Diferença em milissegundos
-        const daysAgo = diffTime / (1000 * 60 * 60 * 24); // Converte a diferença para dias
-        // Se for hoje
-        if (daysAgo === 1) {
+        const diffDays = diffTime / (1000 * 60 * 60 * 24); // Converte a diferença para dias
+    
+        // Se a diferença for 0, significa que é hoje
+        if (diffDays === 1) {
             return 'Hoje';
         }
-
-        // Se for ontem
-        if (daysAgo === 2) {
+    
+        // Se a diferença for 1, significa que é ontem
+        if (diffDays === 2) {
             return 'Ontem';
         }
-
-        if (completeDay === true) {
-
-            // Caso contrário, retorna o dia da semana (Seg, Ter, Qua, etc)
-            const daysOfWeek = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
-            const dayOfWeek = dayDate.getDay(); // Pega o índice do dia da semana (0 - 6)
-            const dayOfMonth = dayDate.getDate(); // Pega o dia do mês
-
-            return `${daysOfWeek[dayOfWeek]} ${dayOfMonth + 1}`; // Exemplo: "Seg 25", "Dom 24", etc.
-
+    
+        // Se for outro dia, retorna o nome do dia da semana
+        const daysOfWeek = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
+        const daysOfWeekAbbr = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']; // Abreviado
+    
+        const dayOfWeek = dayDate.getDay(); // Pega o dia da semana (0 - 6)
+        const dayOfMonth = dayDate.getDate(); // Pega o dia do mês
+    
+        if (completeDay) {
+            // Se completeDay for true, retorna o nome completo do dia (ex: "Segunda-feira 25")
+            return `${daysOfWeek[dayOfWeek]} ${dayOfMonth + 1}`;
         } else {
-
-            // Caso contrário, retorna o dia da semana (Seg, Ter, Qua, etc)
-            const daysOfWeek = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
-            const dayOfWeek = dayDate.getDay(); // Pega o índice do dia da semana (0 - 6)
-            const dayOfMonth = dayDate.getDate(); // Pega o dia do mês
-
-            return `${daysOfWeek[dayOfWeek]} ${dayOfMonth + 1}`; // Exemplo: "Seg 25", "Dom 24", etc.
-
+            // Caso contrário, retorna o nome abreviado do dia (ex: "Seg 25")
+            return `${daysOfWeekAbbr[dayOfWeek]} ${dayOfMonth + 1}`;
         }
     };
 
     const getHourLabel = (dateStr) => {
         const dayDate = new Date(dateStr); // Data recebida
+    
+        const hourOfDay = dayDate.getHours(); // Pega a hora
+        const minutesOfDay = dayDate.getMinutes(); // Pega os minutos
+    
+        // Adiciona um zero à esquerda caso a hora ou os minutos sejam menores que 10
+        const formattedHour = hourOfDay < 10 ? `0${hourOfDay}` : hourOfDay;
+        const formattedMinutes = minutesOfDay < 10 ? `0${minutesOfDay}` : minutesOfDay;
+    
+        return `${formattedHour}:${formattedMinutes}`;
+    };
 
-        const hourOfDay = dayDate.getHours() // Pega o índice do dia da semana (0 - 6)
-        const minutesOfDay = dayDate.getMinutes()
-        return `${hourOfDay}:${minutesOfDay}`
-    }
     // Funções de navegação
     const handleArrowDown = () => {
         console.log("Seta para baixo pressionada");
-        console.log("meu difre", divRef.current[1])
 
-        if (containerCount === 1) {
+        if(containerCount === 0) {
+            let nextCardIndex = buttonCount + 1
+            
+            if(divRef.current[containerCount] && divRef.current[containerCount][nextCardIndex]) {
+                setButtonCount(nextCardIndex);
+                divRef.current[containerCount][nextCardIndex].focus()
+
+                setTimeout(() => {
+                    const scrollStep = 150 * dateFocusedRef.current; // Usa o valor mais recente de dateFocused
+                    const refButtonFocused = scrollRef.current[0][dateFocusedRef.current -1]
+    
+                    if (refButtonFocused) {
+                        const elementRect = refButtonFocused.getBoundingClientRect();
+                        const distanceFromTopOfContent = (elementRect.top + scrollRef.current.scrollTop);
+                        scrollRef.current.scrollBy({
+                            top: distanceFromTopOfContent, // Rola a div para baixo no valor calculado de scrollStep
+                            behavior: 'smooth' // Adiciona rolagem suave
+                        });
+                    }
+                        //const lastElement = buttonRefs.current[containerCount + 1].getAttribute('id')
+
+                        // Distância do topo da div até o topo do elemento focado no conteúdo total
+
+                }, 100);
+            }
+
+
+        
+
+        }else if (containerCount === 1) {
             console.log("opa")
                 let nextCardIndex = cardCount + 1;
                 if (divRef.current[containerCount] && divRef.current[containerCount][nextCardIndex]) {
@@ -219,14 +303,41 @@ function DetailCatchup() {
 
     const handleArrowUp = () => {
         console.log("Seta para cima pressionada");
+        if(containerCount === 0){
+            let previousCardIndex = buttonCount - 1;
+
+            if(divRef.current[containerCount] && divRef.current[containerCount][previousCardIndex]) {
+                setButtonCount(previousCardIndex)
+                divRef.current[containerCount][previousCardIndex].focus()
+            }
+        }else if(containerCount === 1) {
+            let previousCardIndex = cardCount - 1;
+            if (divRef.current[containerCount] && divRef.current[containerCount][previousCardIndex]) {
+                setCardCount(previousCardIndex);
+                divRef.current[containerCount][previousCardIndex].focus();
+            }
+        }
     };
 
     const handleArrowLeft = () => {
-
+        if(containerCount > 0) {
+            let previousCardIndex = containerCount - 1
+            setContainerCount(previousCardIndex)
+            if(divRef.current[previousCardIndex] && divRef.current[previousCardIndex][buttonCount]) {
+                
+                divRef.current[previousCardIndex][buttonCount].focus()
+            }
+        }
     };
 
     const handleArrowRight = () => {
-
+        if(containerCount < 1) {
+            let nextCardIndex = containerCount + 1
+            setContainerCount(nextCardIndex)
+            if(divRef.current[nextCardIndex] && divRef.current[nextCardIndex][cardCount]) {
+                divRef.current[nextCardIndex][cardCount].focus()
+            }
+        }
     };
 
     const handleEnter = () => {
@@ -272,7 +383,6 @@ function DetailCatchup() {
                 <div className="flex container flexColumn declaredOverflow" ref={divRef}
                 >
 
-                    {console.log("meu focused", focusedContent)}
                     <div
                         style={{
                             backgroundImage: `linear-gradient(to right, rgba(17, 16, 20, 1) 0%, rgba(17, 16, 20, 0.9) 30%, rgba(17, 16, 20, 0.1) 100%), url(${focusedContent.image_widescreen !== null ? focusedContent.image_widescreen : focusedContent.image})`,
@@ -322,12 +432,24 @@ function DetailCatchup() {
                         <div className="fixedEventsContainer" ref={divRef}>
                             <div className="fixedDate">
                                 {eventChannels.map((item, idx) => {
-
+                                            const totalPreviousDataLength = eventChannels
+                                            .slice(0, idx)  // Pega todos os itens antes do índice atual
+                                            .reduce((acc, currentItem) => acc + currentItem.data.length, 0);
+                                    
                                     return (
                                         <button
                                         ref={handleOtherButtonRef}
                                             key={idx}
                                             className="fixedDateButton"
+                                            onFocus={(() => {
+                                                handleFocus(idx)
+                                                //setIndividualDataLength(item.data.length)
+                                                //setDateIndex(idx)
+                                            })}
+
+                                            style={{
+                                                backgroundColor: dateIndex === idx  ? "red" : "rgba(0, 0, 0, 0)"
+                                            }}
                                         >
                                             <h3>{getDayLabel(item.day, false)}</h3>
                                         </button>
@@ -336,7 +458,7 @@ function DetailCatchup() {
                                 <h3></h3>
                             </div>
 
-                            <div className="fixedEventDetail">
+                            <div className="fixedEventDetail" ref={scrollRef}>
                                 {eventChannels.map((eventChannel, idx) => {
 
                                     return (
@@ -357,6 +479,7 @@ function DetailCatchup() {
                                                             ref={handleEventButtonRef}                                                            
                                                             className="eventInfoBgdButton"
                                                             onFocus={(() => {
+                                                                setDateIndex(idx)
                                                                 setFocusedContent(eventInfo)
                                                             })}
                                                         >
