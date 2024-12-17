115afe7d4c53b80ad4f2e9e25ace8dcd0f0cb642
Vinícius Okaeda <61584826+ViniciusOkaeda@users.noreply.github.com>
2024-12-17 20:59:51 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://hospitality.youcast.tv.br/",
+    baseURL: "",
     headers: {
       "Content-Type": "application/json",
     }
